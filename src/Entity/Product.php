6bf989c001f64a201077e7c93ef3017f7e36e1a6
akenproger <akenproger@gmail.com>
2022-12-24 13:24:54 +0600

    added api resource

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -3,13 +3,13 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Get;
 use App\Repository\ProductRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[ApiResource]
 class Product
 {
     #[ORM\Id]
